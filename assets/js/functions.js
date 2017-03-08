714bb9f26b78f839f0c238807f06eed2afef151a
Daniel Pichardo <danielpichardo716@gmail.com>
2017-03-08 19:16:40 +0000

    Finished main functionality of site

--- assets/js/functions.js
+++ assets/js/functions.js
@@ -2,6 +2,8 @@
 
 $(function(){
   setInterval(function(){articleTada()}, 4000);
+  mobileNav();
+  smoothScroll();
 });
 
 
@@ -9,6 +11,29 @@ $(window).scroll(function(){
   projectScroll();
   startArticles();
 });
+
+
+function mobileNav() {
+  $('.mobile-nav-toggle').on('click', function(){
+    var status = $(this).hasClass('is-open');
+    if(status){ $('.mobile-nav-toggle, .mobile-nav').removeClass('is-open'); }
+    else { $('.mobile-nav-toggle, .mobile-nav').addClass('is-open'); }
+  });
+}
+
+function smoothScroll (duration) {
+	$('a[href^="#"]').on('click', function(event) {
+
+	    var target = $( $(this).attr('href') );
+
+	    if( target.length ) {
+	        event.preventDefault();
+	        $('html, body').animate({
+	            scrollTop: target.offset().top
+	        }, duration);
+	    }
+	});
+}
 
 function projectScroll() {
   var wScroll = $(window).scrollTop();
